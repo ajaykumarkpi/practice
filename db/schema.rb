f4c3c168eb248f2da10bbb7729bd6e3a22ea6951
ajaykumarkpi <ajay.kumar@kpitechservices.com>
2023-03-29 16:16:57 +0530

    Integrated skills via model generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_29_095322) do
+ActiveRecord::Schema.define(version: 2023_03_29_103924) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

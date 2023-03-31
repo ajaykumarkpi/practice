35a5a0f60b019ace9dcc430199d233168617412a
ajaykumarkpi <ajay.kumar@kpitechservices.com>
2023-03-31 19:06:44 +0530

    Implemented concern for managing Image data

--- app/models/portfo.rb
+++ app/models/portfo.rb
@@ -1,11 +1,10 @@
 class Portfo < ApplicationRecord
+	include Placeholder
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image ||= "http://placehold.co/600x400"
-		self.thumb_image ||= "http://placehold.co/300x250"
+		self.main_image ||= Placeholder.image_generator(height: '600',width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '300',width: '250')
 	end
 
-	if self.main_image == nil
-		self.main_image = "http://placehold.co/600x400"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_31_073633) do
+ActiveRecord::Schema.define(version: 2023_03_31_131902) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2023_03_31_073633) do
     t.integer "percent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|

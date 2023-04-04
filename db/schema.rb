d9d19b93bc75c6b827c13ea469baae63e603fc36
ajaykumarkpi <ajay.kumar@kpitechservices.com>
2023-04-04 13:34:12 +0530

    changed schema file for changes

--- db/schema.rb
+++ db/schema.rb
@@ -20,13 +20,9 @@ ActiveRecord::Schema.define(version: 2023_03_30_085601) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
-
     t.integer "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
     t.index ["topic_id"], name: "index_blogs_on_topic_id"
-
-    t.index ["slug"], name: "index_blogs_on_slug", unique: true
-
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|

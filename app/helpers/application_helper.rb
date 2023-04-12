e7e94bde672cecfda894f5c7e613c97bfdffe578
ajaykumarkpi <ajay.kumar@kpitechservices.com>
2023-04-12 19:13:33 +0530

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
             content_tag(:p, greeting, class: "source-greeting")
         end
     end
+
+    def copyright_generator
+        KpiViewTool::Renderer.copyright 'Ajay kumar','All rights reserved'
+    end
 end
